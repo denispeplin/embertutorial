8c62a13b3dbabcee9311127a542572b6026d2ac6
denispeplin <denis.peplin@gmail.com>
2013-10-19 09:42:42 +0400

    New Rails 4 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embertutorial::Application.config.secret_key_base = '5a7e53da00a8156ac72743d0e5765ff646cf97d8ee2fd43a08a8f2e3a817ff644de48af23c7c704b7928b478685faed194f931c2ea9049595bc22c834c3f3c0c'
